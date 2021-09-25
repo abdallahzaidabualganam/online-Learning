5f674ca58a7caa32c68518b9d8d3023b3d79df41
abdallahzaidabualganam <91079404+abdallahzaidabualganam@users.noreply.github.com>
2021-09-25 13:28:51 +0300

    Add files via upload

---
+++ JS/account.js
@@ -0,0 +1,29 @@
+let name =  document.getElementsByClassName('input-name')
+let pass =  document.getElementsByClassName('input-pass')
+let login = false;
+const url = "http://localhost:4000/api/acount"
+
+async function creatacount(name, pass) {
+    login = true
+    const data = await fetch(url, {
+        method: "POST",
+        headers: {
+            'accept': 'application/json',
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({ name, pass })
+})
+const updated_date = await data.json()
+return updated_date
+}
+
+function right() {
+let display =``
+if ( login == true ) {
+    display +=`<span>${data.name}</span>`
+}
+else{
+    display +=`<span>"login"</span>` 
+}
+document.getElementsByName("sign-in").innerHTML = display
+}
